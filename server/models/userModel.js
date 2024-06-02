41b3de599c255e072c5d2e74afb1c0b39efc97de
Hobey Padlan <hobeypadlanxx@gmail.com>
2024-06-02 22:21:00 +0800

    updated userSchema

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -8,6 +8,71 @@ const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   contact_number: { type: String },
+  personal_information: {
+    name: {
+      last_name: { type: String, maxlength: 100, required: true },
+      first_name: { type: String, maxlength: 100, required: true },
+      middle_name: { type: String, maxlength: 100, required: true },
+      suffix: { type: String, maxlength: 20 }
+    },
+    address: {
+      region: { type: String, maxlength: 100 },
+      province: { type: String, maxlength: 100 },
+      city: { type: String, maxlength: 100 },
+      district: { type: String, maxlength: 100 },
+      barangay: { type: String, maxlength: 100 },
+      residence: { type: String, maxlength: 300, required: true },
+      street: { type: String, maxlength: 100 }
+    },
+    birthdate: { type: Date, required: true },
+    age: { type: Number },
+    marital_status: { type: String, enum: ["Single", "Married", "Widowed", "Legally Separated"] },
+    sex: { type: String, enum: ["Male", "Female", "Prefer not to say", "Other"] },
+    place_of_birth: { type: String, required: true, maxlength: 200 },
+    contact_one: { type: String, maxlength: 50 },
+    contact_two: { type: String, maxlength: 50 },
+    messenger_name: { type: String, maxlength: 100 },
+    religion: { type: String, maxlength: 50 },
+    language_spoken: { type: String, maxlength: 50 },
+    tin: { type: String, maxlength: 50 },
+    gsis_or_sss: { type: String, maxlength: 50 }
+  },
+  family: {
+    name_of_spouse: {
+      last_name: { type: String, maxlength: 100 },
+      first_name: { type: String, maxlength: 100 },
+      middle_name: { type: String, maxlength: 100 },
+      suffix: { type: String, maxlength: 20 }
+    },
+    name_of_father: {
+      last_name: { type: String, maxlength: 100 },
+      first_name: { type: String, maxlength: 100 },
+      middle_name: { type: String, maxlength: 100 },
+      suffix: { type: String, maxlength: 20 }
+    },
+    name_of_mother: {
+      last_name: { type: String, maxlength: 100 },
+      first_name: { type: String, maxlength: 100 },
+      middle_name: { type: String, maxlength: 100 },
+      suffix: { type: String, maxlength: 20 }
+    }
+  },
+  education: {
+    highest_educational_attainment: { type: String, maxlength: 50 },
+    technical_skills: { type: String, maxlength: 400 }
+  },
+  economic_profile: {
+    source_of_income_and_assistance: { type: String, maxlength: 200 },
+    monthly_income: { type: Number }
+  },
+  health_profile: {
+    medical_concern: { type: String, maxlength: 100 },
+    dental_concern: { type: String, maxlength: 100 },
+    social_or_emotional: { type: String, maxlength: 100 },
+    health_problems_or_ailment: { type: String, maxlength: 100 },
+    visual_or_hearing_condition: { type: String, maxlength: 100 },
+    area_of_difficulty: { type: String, maxlength: 100 }
+  }
 });
 
 

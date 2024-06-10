4bbb19c3a1e83b435a7e497f98e4e6b9943540f4
Hobey Padlan <hobeypadlanxx@gmail.com>
2024-06-10 17:11:11 +0800

    updated useSignup

--- client/src/hooks/useSignup.jsx
+++ client/src/hooks/useSignup.jsx
@@ -6,14 +6,14 @@ export const useSignup = () => {
   const [isLoading, setIsLoading] = useState(null)
   const { dispatch } = useAuthContext()
 
-  const signup = async (email, password) => {
+  const signup = async (user) => {
     setIsLoading(true)
     setError(null)
 
     const response = await fetch(import.meta.env.VITE_SERVER + '/user/signup', {
       method: "POST",
       headers: {'content-Type': 'application/json'},
-      body: JSON.stringify({email, password})
+      body: JSON.stringify(user)
     })
     const json = await response.json()
 

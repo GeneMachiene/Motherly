66d43bc6a8b8d2a3a27fc07d7a4adf9e1754d489
Hobey Padlan <hobeypadlanxx@gmail.com>
2024-06-24 17:06:22 +0800

    minor location routes fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,10 @@ import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import Register from "./pages/Register";
 import Signup from "./pages/Signup";
-import Region from "./pages/locations/Region";
-import Province from "./pages/locations/Province";
-import City from "./pages/locations/City";
-import Barangay from "./pages/locations/Barangay";
+import Regions from "./pages/locations/Region";
+import Provinces from "./pages/locations/Province";
+import Citys from "./pages/locations/City";
+import Barangays from "./pages/locations/Barangay";
 
 function App() {
   const currentURI = window.location.href.split("/").at(-1);
@@ -37,10 +37,10 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Register />} />
             <Route path="/profile" element={<Profile />} />
-            <Route path="/locations/regions" element={<Region />} />
-            <Route path="/locations/provinces" element={<Province />} />
-            <Route path="/locations/cities" element={<City />} />
-            <Route path="/locations/barangays" element={<Barangay />} />
+            <Route path="/locations/regions" element={<Regions />} />
+            <Route path="/locations/provinces" element={<Provinces />} />
+            <Route path="/locations/cities" element={<Citys />} />
+            <Route path="/locations/barangays" element={<Barangays />} />
           </Routes>
         </div>
       </BrowserRouter>

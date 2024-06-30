5ca1bfedcf42e5a84a3f756874279b70c7d141d5
Hobey Padlan <hobeypadlanxx@gmail.com>
2024-06-30 22:39:06 +0800

    added appointmentSchema

---
+++ server/models/appointmentModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const appointmentSchema = new Schema(
+  {
+    datetime_of_appointment: { type: Date, required: true },
+    purpose: { type: String, required: true, minlength: 1, maxlength: 1000 },
+    status: {
+      type: String,
+      enum: ["Pending", "Finished", "Cancelled"],
+      default: "Pending",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Booking", appointmentSchema);
